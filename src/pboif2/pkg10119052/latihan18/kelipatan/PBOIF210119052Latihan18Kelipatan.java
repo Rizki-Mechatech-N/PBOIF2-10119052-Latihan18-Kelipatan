6b8a29febfba27fcb7cc73935919bb4f2a63032b
Kiki <Kiki@Kiki-PC>
2020-10-25 10:46:13 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan18/kelipatan/PBOIF210119052Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+package pboif2.pkg10119052.latihan18.kelipatan;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * kelipatan menggunakan perulangan for
+ * 
+ */
+public class PBOIF210119052Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int k = 7;
+        int u = 10;
+        int a = 1;
+        int i = 0;
+       
+        for(i=0;i<u;i++){
+           System.out.println(a*k*0.5);
+           a++;
+        }
+    }
+    
+}
